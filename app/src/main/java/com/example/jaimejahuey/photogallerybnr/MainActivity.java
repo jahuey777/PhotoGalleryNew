74270631a4f0d1dd69d8f04da4ac9edc63467baf
jaimejahuey <jahuey777@hotmail.com>
2017-11-02 13:27:26 -0400

    Finished chapter 23

--- app/src/main/java/com/example/jaimejahuey/photogallerybnr/MainActivity.java
+++ app/src/main/java/com/example/jaimejahuey/photogallerybnr/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.jaimejahuey.photogallerybnr;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SingleFragmentActivity {
+
+    //Api Key 1888392a573dd1ebcf08ab8b8f9aa480
+    //Secret 71987592e9d4766e
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public Fragment createFragment() {
+        return new PhotoGalleryFragment();
     }
+
 }
+
